46f92b1bd0e4107e6dd4ad0e45389fa2e4f8067c
SungMyoungSu <hydra0510@gmail.com>
2020-03-07 01:11:58 +0900

    git ignore 등록

---
+++ src/test/java/com/politics/trend/web/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.politics.trend.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello_return() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+
+    }
+
+
+
+}
