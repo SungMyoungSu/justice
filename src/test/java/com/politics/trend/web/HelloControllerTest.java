2e30f25b0be395bd4f01ac5fce11f19fe58c9b11
SungMyoungSu <hydra0510@gmail.com>
2020-03-08 00:18:36 +0900

    git ignore 등록

--- src/test/java/com/politics/trend/web/HelloControllerTest.java
+++ src/test/java/com/politics/trend/web/HelloControllerTest.java
@@ -1,15 +1,17 @@
 package com.politics.trend.web;
 
+import org.hamcrest.core.Is;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -25,6 +27,18 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+
+    }
+
+    @Test
+    public void hello_Dto_return() throws Exception {
+        String name = "smith";
+        int amount = 2;
+
+        mvc.perform(MockMvcRequestBuilders.get("/hello/dto")
+                .param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(jsonPath("$.name").value(name))
+                .andExpect(jsonPath("$.amount").value(amount));
 
     }
 
