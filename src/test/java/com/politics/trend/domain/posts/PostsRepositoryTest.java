a6aa673e2dfe6970ec51ad7b7e9a185ce2c6a77d
SungMyoungSu <hydra0510@gmail.com>
2020-03-10 23:51:28 +0900

    Posts Test

---
+++ src/test/java/com/politics/trend/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.politics.trend.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void bbs_save_load() {
+        //given
+        String title = "Test bbs title";
+        String contents = "Test bbs contents";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(contents)
+                .author("hydra0510@gamil.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(contents);
+
+
+
+    }
+}
